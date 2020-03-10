8162b5a81eb795497133c3a2f8f9852f15a7ef6d
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-03-10 18:16:59 -0400

    Add original files and finish debugging JavaScript

---
+++ script-original.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    let cupsDrank, response;
+    cupsDrank = document.getElementById("cupsDrank".value;
+    
+    if (cups < 4) {
+        response = "You are fine, maybe have another cup!"
+    }
+
+    else if (cupsDrank >= 4) {
+        talk = "Run to the bathroom NOW!;  
+    }
+
+    console.log(response);
+}
---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    let cupsDrank, response;
+    cupsDrank = document.getElementById("cupsDrank").value;
+    
+    if (cupsDrank < 4) {
+        response = "You are fine, maybe have another cup!";
+    }
+
+    else if (cupsDrank >= 4) {
+        response = "Run to the bathroom NOW!";
+    }
+
+    document.getElementById("result").innerHTML = response;
+
+    console.log(response);
+}
