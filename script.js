1f37041516538962efe4033d80b4a3a4d3540b19
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-03-10 18:28:03 -0400

    Add comments to describe the changes made after debugging

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function myFunction() {
     let cupsDrank, response;
-    cupsDrank = document.getElementById("cupsDrank").value;
+    cupsDrank = document.getElementById("cupsDrank").value; // moved .value outside parentheses, with help from Clyde since we hadn't covered this in class yet
     
-    if (cupsDrank < 4) {
+    if (cupsDrank < 4) { // wrong variable name, changed cups to cupsDrank
         response = "You are fine, maybe have another cup!";
     }
 
     else if (cupsDrank >= 4) {
-        response = "Run to the bathroom NOW!";
+        response = "Run to the bathroom NOW!"; // wrong variable name, changed talk to response
     }
 
-    document.getElementById("result").innerHTML = response;
+    document.getElementById("result").innerHTML = response; // added this line of code so response would appear on index.html webpage
 
     console.log(response);
 }
